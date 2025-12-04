fa63309b8447d991af75ce4c658fabab57e27047
Aliaksandr Pyrkh <alexpyrkh@gmail.com>
2025-12-04 14:48:43 +0100

    Fixed deprecated code usage

--- src/graph/state.ts
+++ src/graph/state.ts
@@ -2,8 +2,8 @@ import { Annotation } from "@langchain/langgraph";
 import { BusinessContext } from "../schemas/business_context";
 import { ExpandedContext } from "../schemas/expanded_context";
 
-export const GraphState = {
+export const GraphState = Annotation.Root({
   rawInput: Annotation<string>(),
   businessContext: Annotation<BusinessContext | null>(),
   expandedContext: Annotation<ExpandedContext | null>(),   // ← новое поле
-};
+});
